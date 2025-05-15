5aed12815593b5fdbabefb33e6935793305f5a25
M H <mlekalwohosh22@gmail.com>
2025-05-15 19:17:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Runtime Error: Error: ./src/app/layout.tsx
    > Module not found: Can't resolve 'next/font/google/target.css'
    > Unknown font
    >
    > Debug info:
    > - Execution of <ModuleAssetContext as AssetContext>::resolve_asset failed
    > - Execution of resolve failed
    > - Execution of resolve_internal failed
    > - Execution of NextFontGoogleReplacer::import_map_result failed
    > - Execution of font_options_from_query_map failed
    > - Unknown font
    > Error while looking up import map: Unknown font
    >
    > Debug info:
    > - Execution of NextFontGoogleReplacer::import_map_result failed
    > - Execution of font_options_from_query_map failed
    > - Unknown font
    >
    >
    > https://nextjs.org/docs/messages/module-not-found
    >
    > . Error source: Call Stack
    > 7
    >
    > Show 7 ignore-listed frame(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from 'next';
-import { Geist_Sans as GeistSans, Geist_Mono as GeistMono } from 'next/font/google';
+import { Inter, Roboto_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { ThemeProvider } from '@/components/ThemeProvider';
 
-const geistSans = GeistSans({
-  variable: '--font-geist-sans',
+const inter = Inter({
+  variable: '--font-inter',
   subsets: ['latin'],
 });
 
-const geistMono = GeistMono({
-  variable: '--font-geist-mono',
+const robotoMono = Roboto_Mono({
+  variable: '--font-roboto-mono',
   subsets: ['latin'],
 });
 
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${inter.variable} ${robotoMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
